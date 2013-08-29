e6730166abfe4f057ab20b847ff5db4e920581df
redbaron76 <f.fumis@gmail.com>
2013-08-29 09:42:57 +0200

    models setup

---
+++ src/Pongo/Cms/Models/Translation.php
@@ -0,0 +1,58 @@
+<?php namespace Pongo\Cms\Models;
+
+use LaravelBook\Ardent\Ardent;
+
+class Translation extends Ardent {
+	
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'translations';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	// protected $hidden = array('password');
+
+	/**
+	 * Allowed mass assignment columns
+	 * 
+	 * @var array
+	 */
+	// protected $fillable = array('username', 'email');
+
+	/**
+	 * Danied mass assignment columns
+	 * 
+	 * @var array
+	 */
+	// protected $guarded = array('id', 'password');
+
+	/* ARDENT */
+
+	/**
+	 * Ardent clear redundant attributes
+	 * 
+	 * @var boolean
+	 */
+	public $autoPurgeRedundantAttributes = true;
+
+	/**
+	 * Ardent validation rules
+	 * 
+	 * @var array
+	 */
+	public static $rules = array(
+
+		/*'username' 				=> 'required|between:4,16',
+		'email'					=> 'required|email',
+		'password'				=> 'required|alpha_num|between:4,8|confirmed',
+		'password_confirmation' => 'required|alpha_num|between:4,8'*/
+
+	);
+
+}
